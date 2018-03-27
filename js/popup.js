5a2418cd3fe1d4e1e9c65826adc11695fa06122a
Alexander <borom@inbox.ru>
2018-03-27 10:48:36 +0300

    Создан файл скриптов

---
+++ js/popup.js
@@ -0,0 +1,7 @@
+
+var popup = document.querySelector(".popup.write-us");
+var map = document.querySelector(".popup.map");
+var writeus = document.querySelector(".write");
+writeus.addEventListener("click", function () {
+	popup.classList.add("popup-open");
+}
